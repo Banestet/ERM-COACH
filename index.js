e6acf7ced30626982cd99121a9489b5777ba9c44
Esteban Rodriguez <Resteban368@gmail.com>
2020-06-17 15:09:21 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+
+function enviarDatos() {
+    console.log('enviando Datos...');
+    window.location.href = 'index2.html';
+    alert("welcome FORZA");
+    return false;
+
+}
